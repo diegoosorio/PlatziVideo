672051da5f93f89002d68b399bb63459cf9b6701
Diego Osorio <diegoso-0989@hotmail.com>
2019-10-29 20:32:51 -0500

    Manejando el estado en Login

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,7 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
+// import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 
